d188e1063aa08c1d430e7c29c2d714eff3e5d789
Trevor Alexander <talex@privatdemail.net>
2014-03-28 01:59:10 -1000

    Table download works, jitter affects table also now.
    Info page with licensing also added. Improved the formatting a little bit, as well as increasing the plot size. The only things left to do (for now) are table download and session tracking.

---
+++ readme.md
@@ -0,0 +1,42 @@
+Info
+------------
+
+### What is this?
+
+Fuzzy Locator is a toy app that demonstrates the concept of *differential privacy* in announcing your location.  As Cynthia Dwork [showed in 2006](http://www.msr-waypoint.com/pubs/64346/dwork.pdf), it is impossible to create a database that is of use to anybody and guarantee *absolute privacy* at the same time. One approach to mitigating this risk is adding exponentially distributed noise to the data, as mentioned in the paper.
+
+### Why?
+
+Back in the 1990s, when the Internet was wild and free, kids (and adults, and aliens, and creatures from Beyond) would get on relatively anonymous chats like IRC and speak pretty frankly about whatever they were thinking, in whatever chatroom they happened to be in at the time. Just about nobody used their "real" names, though, and there weren't many data mining algorithms in play to pry that information out of their speaking style. One way to get to know somebody was to ask, `a/s/l?`. This was short for "what's your age, sex, and location?" Naturally, you were free to answer however you liked, which made it that much more interesting.
+
+Nowadays multinational conglomerates demand "real" names from their users, mainly for the purpose of recording/predicting their behavior and trying to get them to buy stuff. Your IP address gives away your location, [typically within a few hundred meters](http://www.ipfingerprints.com/). Algorithms have been developed to [figure out your gender](http://www.hackerfactor.com/GenderGuesser.php) (whatever that means) from the words you use.
+
+There's not much room left for imagination these days.
+
+If you *do* hide your IP address through something like [Tor](https://www.torproject.org/), or you're exploring a different identity, something like this tool might help bring some of that back.
+
+### How it works
+
+If you click somewhere on the map, and hit the "Draw Circle" button, a semi-transparent circle will appear near where you clicked. Specifically, it will have a radius as big as you set with the radius slider, and it will be randomly scooted in some direction according to the amount of noise you add with the jitter slider. You can pick whatever color feels right, too.
+
+Feel free to click around and play with the controls. No matter how you set the jitter or the radius, the circle will always contain the location you clicked on initially. (The circle will never lie if you don't.)
+
+Once you're happy with the result, you can enter whatever (pseudo)nym you like and any kind of contact info into the two text fields. If you hit "Submit Info", the position and radius of the circle will be added to the List tab. You can download the table as well as the map, if you like.
+
+### Licensing
+This application and all original code used therein are licensed under the GPLv3.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+**Fuzzy Locator** copyright (C) 2014 Trevor Alexander
--- server.R
+++ server.R
@@ -1,8 +1,6 @@
 library(shiny)
 library(maps)
 library(mapdata)
-library(ggplot2)
-library(ggthemes)
 library(rworldmap)
 library(gridExtra)
 source("./mapfuncs.R")
@@ -15,79 +13,61 @@ locationtable <- data.frame(Nym=character(),
                             Radius=numeric())
 
 shinyServer(function(input, output) {
-
+#     # Reactive dependencies
+    buttonClicked <- reactive(input$paintupdate)
+    make.noise <- reactive({
+      if(buttonClicked() > 0) {
+        # Every time the update button is clicked, we draw another noise pair
+        # We use two uniform variables to generate the signs for a laplace 
+        # distribution.
+        x.sign <- sign(runif(1,min=-1,max=1))
+        y.sign <- sign(runif(1,min=-1,max=1))
+        # The shift created by the noise is bounded by the radius.
+        # This ensures the app never really "lies".
+        x.jitter <- x.sign*min(rexp(1,rate=1/input$noisepow),input$radius) *
+          sqrt(2)/2
+        y.jitter <- y.sign*min(rexp(1,rate=1/input$noisepow),input$radius) *
+          sqrt(2)/2
+        print(x.jitter)
+        print(y.jitter)
+        return(data.frame(x.jitter = x.jitter, y.jitter = y.jitter))
+      }
+    })
+        
+    #  Map plot
     #  1) It is "reactive" and therefore should be automatically 
     #     re-executed when inputs change
     #  2) Its output type is a plot 
     #
-    output$myworld <- renderPlot({
-        # Reactive dependencies
-        input$paintupdate
-        # Every time the update button is clicked, we draw another noise pair
-        # We use two uniform variables to generate the signs for a laplace distribution.
-        x.sign <- sign(runif(1,min=-1,max=1))
-        y.sign <- sign(runif(1,min=-1,max=1))
-        x.jitter <- x.sign*min(rexp(1,rate=1/input$noisepow),input$radius)*sqrt(2)/2
-        y.jitter <- y.sign*min(rexp(1,rate=1/input$noisepow),input$radius)*sqrt(2)/2
+    theworld <- reactive({
+      buttonClicked()  
+      isolate({
+        par(mar = rep(0,4))
+        map("world2", projection="vandergrinten", wrap=TRUE, 
+            resolution=2, mar = rep(0,4),
+            bg=rgb(210, 200, 176,alpha=255,maxColorValue = 255))
+      })
         
-        # generate an rnorm distribution and plot it
-        # dist <- rexp(input$obs)
-
-
-        # world <- map_data("world")
-        # worldmap <- ggplot(aes(x = long, y = lat, group = group), data = world) +
-        #    geom_path() # +
-        # coord_map("vandergrinten")
-
-        # Builds offset map
-        # mp1 <- fortify(map(fill=TRUE, plot=FALSE))
-        # mp2 <- mp1
-        # mp2$long <- mp2$long + 360
-        # mp2$group <- mp2$group + max(mp2$group) + 1
-        # mp <- rbind(mp1, mp2)
-        # worldmap <- ggplot(aes(x = long, y = lat, group = group), data = mp) + 
-        #     # geom_polygon() +
-        #     geom_path() + # colour="grey") +
-        #     scale_x_continuous(limits = c(-26, 334)) + # sets the longitudinal center
-        #     # coord_map("cylequalarea",lat0=90)
-        #     # coord_map("vandergrinten") +
-        #     # scale_y_reverse() + 
-        #     theme_solarized(light=TRUE)
-
-
-        # if ("plotclick" %in% names(input)) {
-        #cur.circ <- circleFun(input$plotclick,100,npoints = 100)
-        #worldmap <- worldmap +
-        #    annotate("path",
-        # x=input$plotclick$x+10*cos(seq(0,2*pi,length.out=100)),
-        # y=input$plotclick$y+10*sin(seq(0,2*pi,length.out=100)))
-        # geom_polygon(aes(cur.circ$x, cur.circ$y), data = df, inherit.aes = F)
-        #}
-        
-        # print(worldmap)
-        isolate(map("world2", projection="vandergrinten", wrap=TRUE, resolution=2,
-            bg=rgb(210, 200, 176,alpha=255,maxColorValue = 255)))
-
-        # worldmap <- ggplot(aes(x = mapdata$x, y = mapdata$y)) + geom_path()
-        # print(worldmap)
-
-        
-        isolate({
-            # The shift created by the noise is bounded by the radius.
-            # This ensures the app never really "lies".
-            noisycenter <- c(input$plotclick$x+x.jitter,
-                             input$plotclick$y+y.jitter)
-            cur.circ <- circleFun(noisycenter,
-                                  radius=input$radius, npoints = 30)
-            
-            polygon(x=cur.circ$x,y=cur.circ$y,col = rainbow(1000,s=0.5,alpha=0.5)[input$circlecolor])
-            if("plotclick" %in% names(input)) {
-                text(labels=input$name,x=noisycenter[1],y=noisycenter[2],
-                     vfont=c("sans serif","plain"), cex=10*log(input$radius*5))
-            }
-        })
-        
+      isolate({
+        new.noise <- make.noise()
+        noisy.coords <- data.frame(x = input$plotclick$x+new.noise$x.jitter,
+                                   y = input$plotclick$y+new.noise$y.jitter)
+        cur.circ <- circleFun(c(noisy.coords$x,noisy.coords$y),
+                              radius=input$radius, npoints = 30)
+          
+        polygon(x=cur.circ$x,y=cur.circ$y,
+                col = rainbow(1000,s=0.5,alpha=0.5)[input$circlecolor])
+        if("plotclick" %in% names(input)) {
+            text(labels=input$name,x=noisy.coords$x,y=noisy.coords$y,
+                 vfont=c("sans serif","plain"), cex=10*log(input$radius*5))
+        }
+      })
+      
     })
+    
+    output$myworld <- renderPlot({
+      print(theworld())
+    }, height = 640)
 
     output$clickcoord <- renderPrint({
         # get user clicks, report coords
@@ -95,45 +75,65 @@ shinyServer(function(input, output) {
             print(input$plotclick)
         }
     })
-
-    reactive({
-        if (input$submitinfo > 0) {
-            locationtable <<- rbind(locationtable,
-                                   data.frame(Nym=input$name,
-                                              Contact=input$contact,
-                                              Longitude=input$plotclick$x,
-                                              Latitude=input$plotclick$y,
-                                              Radius=input$radius))
+    
+    output$noisycoord <- renderPrint({
+        if ("plotclick" %in% names(input)) {
+            print(make.noise())
         }
     })
 
+    reactive({
+      if (input$submitinfo > 0) {
+          new.noise <- make.noise()
+          locationtable <<- 
+            rbind(locationtable,
+                  data.frame(Nym=input$name,
+                             Contact=input$contact,
+                             Longitude=input$plotclick$x+new.noise$x.jitter,
+                             Latitude=input$plotclick$y+new.noise$y.jitter,
+                             Radius=input$radius))
+      }
+    })
+
     output$locationtable <- renderTable({
-        # if (is.null(locationtable)) {return()}
-        locationtable
-        if (input$submitinfo > 0) {
-            locationtable <<- rbind(locationtable,
-                                    data.frame(Nym=input$name,
-                                         Contact=input$contact,
-                                         Longitude=input$plotclick$x,
-                                         Latitude=input$plotclick$y,
-                                         Radius=input$radius))
-        }
-        print(locationtable)
+      # if (is.null(locationtable)) {return()}
+      new.noise <- make.noise()
+      locationtable
+      if (input$submitinfo > 0) {
+        locationtable <<- 
+          rbind(locationtable,
+                data.frame(Nym=input$name,
+                           Contact=input$contact,
+                           Longitude=input$plotclick$x+new.noise$x.jitter,
+                           Latitude=input$plotclick$y+new.noise$y.jitter,
+                           Radius=input$radius))
+      }
+      print(locationtable)
       }, 'include.rownames' = FALSE
        , 'include.colnames' = TRUE
        , 'sanitize.text.function' = function(x){x}
     )
 
+    output$saveinfo <- downloadHandler(
+      filename = function() {
+          paste('fuzzytable-', Sys.Date(), '.csv', sep="")
+      },
+      content = function(file) {
+        write.csv(locationtable, file)
+      })
 
     output$savemap <- downloadHandler(
-        filename = function() {
-            paste('fuzzymap-', Sys.Date(), '.png', sep="")
-        },
-        content = function(filecon) { # function to actually write the image file
-            # http://stackoverflow.com/questions/14810409/save-plots-made-in-a-shiny-app?rq=1
-            png(file)
-            #print(
-        })
+      filename = function() {
+          paste('fuzzymap-', Sys.Date(), '.png', sep="")
+      },
+      content = function(file) { 
+# function to actually write the image file
+# http://stackoverflow.com/questions/14810409/save-plots-made-in-a-shiny-app?rq=1
+          png(file)
+          myplot <- theworld()
+          print(myplot)
+          dev.off()
+          contentType = 'image/png'
+      })
 
-    # output$savedata
 })
