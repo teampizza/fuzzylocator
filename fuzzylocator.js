d7b19b03bf8c878c443307fb1687ec37e7fd0020
Trevor Alexander <talex@privatdemail.net>
2014-06-11 00:05:19 -1000

    Partial scaffolding of control container
    Contains some non-working code borrowed from Leaflet.LocationShare that
    I am trying to learn from.

--- fuzzylocator.js
+++ fuzzylocator.js
@@ -7,8 +7,38 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // add scale marker
-
 L.control.scale().addTo(map)
 
 // add our menu
-var my
+var FuzzyMenu = L.control.extend({
+		options: {
+				position: 'topright'
+		},
+
+		onAdd: function (map) {
+        // create the control container with a particular class name
+        var container = L.DomUtil.create('div', 'fuzzy-menu');
+
+        // ... initialize other DOM elements, add listeners, etc.
+				// var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
+
+        this.link = L.DomUtil.create('a', 'leaflet-bar-part', container);
+// var userIcon = L.DomUtil.create('i', 'fa fa-users fa-lg', this.link);
+        var userIcon = L.DomUtil.create('img' , 'img-responsive' , this.link);
+        userIcon.src = 'https://raw.githubusercontent.com/CliffCloud/Leaflet.LocationShare/master/dist/images/IconLocShare.png'
+        this.link.href = '#';
+
+        L.DomEvent.on(this.link, 'click', this._click, this);
+
+        return container;
+    },
+
+		_click: function (e) {
+      L.DomEvent.stopPropagation(e);
+      L.DomEvent.preventDefault(e);
+// TODO: get location and putout url
+      placeMarker( this._map )
+		}
+});
+
+map.addControl(new FuzzyMenu());
