39372bf13d327d9d35b8c4a5bf15eb229b5f49be
Trevor Alexander <talex@privatdemail.net>
2014-08-08 20:06:27 -1000

    Cleanup (removed old R/Shiny implementation)

--- shiny/mapfuncs.R
+++
@@ -1,6 +0,0 @@
-circleFun <- function(center = c(0,0),radius = 1, npoints = 100){
-    tt <- seq(0,2*pi,length.out = npoints)
-    xx <- center[1] + radius * cos(tt)
-    yy <- center[2] + radius * sin(tt)
-    return(data.frame(x = xx, y = yy))
-}
